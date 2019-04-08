20060afa1508f136fbeba5a5d7d02a6c4f2f6834
mark-marshall <mark.marshallgp@gmail.com>
2019-04-08 15:54:55 +0100

    [MVP] Added tests for a selection of functions in arrays

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,17 +1,35 @@
 const arrayFunctions = require('./arrays');
 
-// we've gone ahead and gotten a start here for you,
-// except, for some reason, none of our current assertions are working.
-// first step is to make sure all these assertions work.
-// then make sure you have at least 2-3 more assertions for every function.
-// hint 1. - you need to pass data to the functions and get expected output.
-// hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
+const { map, filter,flatten } = arrayFunctions;
 
-// describe('Arrays', () => {
-//   describe('map', () => {
-//     it('should be a function', () => {
-//       const map = arrayFunctions.map;
-//       expect(typeof map).toBe('object');
-//     });
-//   });
-// });
+describe('arrays helpers', () => {
+  describe('map fn', () => {
+    it('should be a function', () => {
+      expect(typeof map).toBe('function');
+    });
+    it('return an array with 1 added to each number', () => {
+      expect(map([1, 2, 3, 4], num => num + 1)).toEqual([2, 3, 4, 5]);
+    });
+  });
+
+  describe('filter fn', () => {
+    it('return a selected number from a list', () => {
+      expect(
+        filter([1, 2, 3, 4], item => {
+          if (item === 1) {
+            return item;
+          }
+        }),
+      ).toEqual([1]);
+    });
+  });
+
+  describe('flatten fn', () => {
+    it('flatten a nested array', () => {
+      expect(flatten([1,2,3,[4,5,6]])).toEqual([1,2,3,4,5,6]);
+    });
+    it('flatten a nested-nested array', () => {
+        expect(flatten([1,2,3,[4,[5,6]]])).toEqual([1,2,3,4,5,6]);
+      });
+  });
+});
